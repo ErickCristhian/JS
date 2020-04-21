2f6b855a9c1746e16df991b13a428b18aeb2f6f3
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-04-21 00:38:02 -0300

    React com Redux

---
+++ React/redux_app/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import { bindActionCreators } from 'redux';
+import { clickHelloAction } from './actions/index';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  render() {
+    const { text } = this.state;
+    const { msg, clickHelloAction } = this.props;
+    return (
+      <div className="App">
+        <input type="text" onChange={this.handleInputChange}/>
+        <button type="button" onClick={ () => clickHelloAction(text) }>Click to Dispatch Action</button>
+        <h1>{ msg }</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (store) => {
+  return {
+    msg: store.clickReducer.msg
+  }
+}
+const mapDispatchToProps = (dispatch) => bindActionCreators({ clickHelloAction }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

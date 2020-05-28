6fec43148e0860bd1c158a7012f65837126e2019
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-05-28 13:38:22 -0300

    Pipefy-React clone Project

---
+++ React/pipefy-react/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+        <h1>Pipefy-React</h1>
+    </Container>
+    );
+}
+
